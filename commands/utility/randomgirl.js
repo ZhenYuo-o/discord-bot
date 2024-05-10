523eb4beb7be3c441a2452bfa1ecd7fa5d0980a6
zhenyu <zzhan@student.tgm.ac.at>
2024-05-11 00:44:05 +0200

    chore(fix animegirl): making default content selection

--- commands/utility/randomgirl.js
+++ commands/utility/randomgirl.js
@@ -20,7 +20,13 @@ module.exports = {
     async execute(interaction) {
         const content = interaction.options.getString('content');
         await interaction.reply({ content: 'Getting cute anime girls....mostly', fetchReply: true });
-        const image = await axios.get(`https://api.nekosapi.com/v3/images/random/file?rating=${content}`, {responseType: 'arraybuffer'});
+        let image
+        if(!content == null) {
+            image = await axios.get(`https://api.nekosapi.com/v3/images/random/file?rating=${content}`, {responseType: 'arraybuffer'});
+        } else {
+            image = await axios.get(`https://api.nekosapi.com/v3/images/random/file?rating=safe`, {responseType: 'arraybuffer'});
+
+        }
         const attachment = new AttachmentBuilder(image.data,'anime_image.png');
         await interaction.editReply({content: 'Tadaa!', files: [attachment]});
     },
